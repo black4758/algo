04a6e3b62ded89f0fc4e5b7d933a9df2f111eb9f
ar2024  git config --global user.email arblack475867@gmail.com <arblack475867@gmail.com>
2024-08-27 15:55:49 +0900

    DFS 와 BFS

--- 240827/DFS_BFS_BOJ_1260.java
+++ 240827/DFS_BFS_BOJ_1260.java
@@ -5,25 +5,23 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class DFS_BFS_BOJ_1260 {
-	static int V;
+	static int N;
 	static int[][]adjMatrix;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		V= sc.nextInt();
-		int E= sc.nextInt();
+		N= sc.nextInt();
+		int M= sc.nextInt();
 		int st= sc.nextInt();
-		adjMatrix= new int[V+1][V+1];
-		for(int i=0;i<E;i++) {
+		adjMatrix= new int[N+1][N+1];
+		for(int i=0;i<M;i++) {
 			int from=sc.nextInt();
 			int to=sc.nextInt();
 			adjMatrix[to][from]=adjMatrix[from][to]=1;
 		}
-//		for(int[] adj : adjMatrix) {
-//			System.out.println(Arrays.toString(adj));
-//		}
+
 		
-		dfs(st,new boolean[V+1]);
+		dfs(st,new boolean[N+1]);
 		System.out.println();
 		bfs(st);
 	}
@@ -33,16 +31,15 @@ public class DFS_BFS_BOJ_1260 {
 			
 		System.out.print((cur+" "));
 			
-		for(int i=1;i<=V;i++) {
+		for(int i=1;i<=N;i++) {
 			if(adjMatrix[cur][i]==0||visited[i]) continue;
 			dfs(i,visited);
-			
 		}
 	}
 	
 	private static void bfs(int start) {
 		Queue<Integer> queue = new ArrayDeque<>();
-		boolean[] visited = new boolean[V+1];
+		boolean[] visited = new boolean[N+1];
 		
 		visited[start]=true;
 		queue.offer(start);
@@ -52,7 +49,7 @@ public class DFS_BFS_BOJ_1260 {
 			
 			System.out.print((cur+" "));
 			
-			for(int i=1;i<=V;i++) {
+			for(int i=1;i<=N;i++) {
 				if(adjMatrix[cur][i]==0||visited[i]) continue;
 				
 				visited[i]=true;
