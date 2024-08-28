602f0ec4034e171ac8d23e2e489d06a1b29017bd
ar2024  git config --global user.email arblack475867@gmail.com <arblack475867@gmail.com>
2024-08-28 13:10:12 +0900

    서로소 집합

---
+++ 240828/SWEA_3289.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SWEA_3289 {
+	static int[] arr;
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int Tc=sc.nextInt();
+		for(int t=1;t<=Tc;t++) {
+			int n=sc.nextInt();
+			int m=sc.nextInt();
+			arr= new int[n+1];
+			System.out.print("#"+t+" ");
+			for(int i=0;i<m;i++) {
+				int st=sc.nextInt();
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+				if(st==0) {
+					union(a,b);
+				}
+				if(st==1) {
+					
+					if(fideSet(a)==fideSet(b)) System.out.print(1);
+					else System.out.print(0);
+				}
+			}
+			System.out.println();
+		}
+	}
+	 static int  fideSet(int a){
+		if(arr[a]==0)return a;
+		return arr[a]=fideSet(arr[a]);
+	}
+	static boolean union(int a, int b) {
+		int aroot=fideSet(a);
+		int broot=fideSet(b);
+		if(aroot==broot) return false;
+		arr[broot]=aroot;
+		return false;
+	}
+}
