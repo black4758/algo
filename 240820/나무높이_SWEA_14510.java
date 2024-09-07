0096e532db3bdc61cff3c3a51da3df1b960b1454
arblack475867 <arblack475867@gmail.com>
2024-09-07 16:55:36 +0900

    나무 높이

---
+++ 240820/나무높이_SWEA_14510.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+
+import java.util.LinkedList;
+
+import java.util.StringTokenizer;
+
+public class 나무높이_SWEA_14510 {
+	 public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 int T= Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int N= Integer.parseInt(br.readLine());
+			int[] map = new int[N];
+			int max=0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				map[i]=Integer.parseInt(st.nextToken());
+				if(map[i]>max) {
+					max=map[i];
+				}
+			}
+			LinkedList<Integer> list = new LinkedList<>();
+			for (int i = 0; i < N; i++) {
+				if(max != map[i]) {
+					list.add(map[i]);
+				}
+			}
+			int day=0;
+
+			while(!list.isEmpty()) {	
+                Collections.sort(list,Collections.reverseOrder());
+				day++;
+				for (int i = 0; i < list.size(); i++) {
+					if(day%2 ==0 && max -list.get(i) !=1) {
+						int now= list.get(i)+2;
+						list.remove(i);
+						if(now != max) list.add(now);
+						break;
+					}
+					if( day%2 ==1 &&(max- list.get(i) !=2 || list.size()>=2)) {
+						int now= list.get(i)+1;
+						list.remove(i);
+						if(now != max) list.add(now);
+						break;
+					}
+				}
+			}
+			System.out.println("#"+tc+" "+day);
+		}
+	}
+}
