a312ab2ea6c129d81abe266b72808050213d6c0e
woo seong yun <arblack475867@gmail.com>
2025-06-26 17:12:14 +0900

    Ladder1

--- 240813/Ladder1_SW1210.java
+++ 240813/Ladder1_SW1210.java
@@ -1,50 +1,55 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+package main.java._2025_06_26;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Ladder1_SW1210
 {
-	static int  [][] arr;
-	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-		int T;
+	static class Position{
+		int x;
+		int y;
 
-		//풀이법 2를 찾은 후 거꾸로 탐색을 하여 값을 찾는다
-		for(int test_case = 1; test_case <= 10; test_case++)
-		{
-			
-			int a= sc.nextInt();
-			int stx = 0,sty=0;
-			arr = new int[100][100];
-			for(int i=0;i<100;i++) {
-				for(int j=0;j<100;j++) {
-					arr[i][j]=sc.nextInt();
-					if(arr[i][j]==2) {
-						stx=i;
-						sty=j;
+		public Position(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	static int[][] map;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		main.java._2025_06_26.Ladder1_SW1210.Position position = null;
+		for (int i = 0; i < 10; i++) {
+			int N= Integer.parseInt(br.readLine());
+			map = new int[100][100];
+			for (int j = 0; j < 100; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < 100; k++) {
+					map[j][k] = Integer.parseInt(st.nextToken());
+					if(map[j][k] == 2){
+						position=new main.java._2025_06_26.Ladder1_SW1210.Position(j, k);
 					}
 				}
 			}
-			solution(a,stx,sty);
-		}
-		
-	}
-	private static void solution(int a, int stx, int sty) {
-		int[] dx= {0,0,-1};
-		int[] dy= {-1,1,0}; //왼쪽 오른쪽 위쪽 순으로 
-		while(stx!=0) {
-			for(int i=0;i<3;i++) {
-				int new_x=stx+dx[i];
-				int new_y=sty+dy[i];
-				if(new_x>=0&&new_x<100&&new_y>=0&&new_y<100&&arr[new_x][new_y]==1) {
-					arr[stx][sty]=10;
-					stx=new_x;
-					sty=new_y;
+			int nx=0;
+			int ny=0;
+			int[] dx= {0,0,-1};
+			int[] dy= {-1,1,0};
+			while (position.x>0){
+				for(int j=0;j<3;j++) {
+					nx=position.x+dx[j];
+					ny=position.y+dy[j];
+					if(nx>=0&&nx<100&&ny>=0&&ny<100&&map[nx][ny]==1){
+						map[nx][ny]=0;
+						position.x=nx;
+						position.y=ny;
+					}
 				}
 			}
+			System.out.println("#"+N+" "+position.y);
 		}
-		System.out.println("#"+a+" "+sty);
 	}
 }
