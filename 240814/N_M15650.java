a381e988fc3c7965a324d2ddba2d834dce455070
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-01 15:41:41 +0900

    백준 15650  N과 M (2)

--- 240814/N_M15650.java
+++ 240814/N_M15650.java
@@ -1,29 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N_M15650 {
-	static int[] inputarr;
-	static int M;
-	static int N;
-	//풀이법 조합을 이용하여 풀이
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N= sc.nextInt();
-		M= sc.nextInt();
-		inputarr = new int[N];
-		johab(0,1);
-	}
-	private static void johab(int cnt, int st) {
-		if(cnt==M) {
-			for(int i=0;i<M;i++) {
-				System.out.print(inputarr[i]+" ");
-			}
-			System.out.println();
-			return ;
-		}
-		for(int i=st;i<=N;i++) {
-			inputarr[cnt]=i;
-			johab(cnt+1,i+1);
-		}
-		
-	}
+    static int[] arr;
+    static int M;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M= Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        combination(0,1);
+    }
+
+    private static void combination(int cnt, int st) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = st; i <=N; i++) {
+            arr[cnt] = i;
+            combination(cnt+1, i+1);
+        }
+    }
 }
