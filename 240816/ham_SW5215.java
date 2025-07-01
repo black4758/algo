d5f9f435b3fac7dc0b1f8486736b43df0e8854f9
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-01 15:46:55 +0900

    SW 5215 햄버거 다이어트

--- 240816/ham_SW5215.java
+++ 240816/ham_SW5215.java
@@ -1,53 +1,41 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileInputStream;
+
+public class ham_SW5215 {
+    static int[][] arr;
+    static int N;
+    static int L;
+    static int re;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int t = 1; t <= T; t++) {
+            N=scanner.nextInt();
+            L=scanner.nextInt();
+            re=0;
+            arr = new int[N][2];
+            for (int i = 0; i < N; i++) {
+                arr[i][0] = scanner.nextInt();
+                arr[i][1] = scanner.nextInt();
+            }
+            subset(0,0,0);
+            System.out.println("#" + t + " " + re);
+        }
+    }
+    private static void subset(int sc, int cal, int cnt ) {
+
+        if(cal>L) {
+            return ;
+        }
+        if (cnt == N) {
+            if (sc > re) {
+                re = sc;
+            }
+            return;
+        }
+
+        subset(sc+arr[cnt][0],cal+arr[cnt][1],cnt+1);
+        subset(sc, cal, cnt+1);
 
 
-public class ham_SW5215
-{
-	static int N,L,arr[][],max;
-	static boolean charr[];
-	public static void main(String args[]) throws Exception
-	{
-
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		// 문제 풀이 부분집합을 이용하여 모든 경우의 수를 구한 뒤 주어진 칼로리 이하의 점수들을 리스트에 집어넣은 후 정렬하여 찾는다
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
-			N= sc.nextInt();
-			L= sc.nextInt();
-			arr= new int [N][2];
-			charr=new boolean [N];
-			for(int i=0;i<N;i++) {
-				for(int j=0;j<2;j++) {
-					arr[i][j]= sc.nextInt();
-				}
-			}
-			max=0;
-			gibhab(0,0,0,0);
-			
-			System.out.println(max);
-
-		}
-	}
-	private static void gibhab(int cnt,int cal,int scol,int eCnt) {
-		if(cal>L) {
-			return ;
-		}
-		if(cnt == N ) {
-			if (eCnt>0 && cal<=L)  {
-				if(max<scol) max=scol;
-			}
-			return ;
-		}
-
-		charr[cnt]= false;
-		gibhab(cnt+1,cal,scol,eCnt);
-		charr[cnt]= true;
-		gibhab(cnt+1,cal+arr[cnt][1],scol+arr[cnt][0],eCnt+1);
-	}
+    }
 }
