b0916d845169ec09af5be94d2bd343c66f3e358b
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-01 16:33:55 +0900

    SW 3234 준환이의 양팔저울

--- 240816/준환이의_양팔저울_SWEA_3234.java
+++ 240816/준환이의_양팔저울_SWEA_3234.java
@@ -1,52 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class 준환이의_양팔저울_SWEA_3234 {
-	static int map[],solArr[],N,answer;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T= Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			
-			N= Integer.parseInt(br.readLine());
-			map = new int[N];
-			solArr= new int[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				map[i]=Integer.parseInt(st.nextToken());
-			}
-			answer=0;
-			sunyal(0,new boolean[N]);
-			System.out.println("#"+tc+ " "+answer);
-		}
-	}
-	private static void sunyal(int cnt, boolean[] visited) {
-		
-		if(cnt==N) {
-			unSet(0,0,0);
-			return;
-		}
-		for (int i = 0; i < visited.length; i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				solArr[cnt]=map[i];
-				sunyal(cnt+1, visited);
-				visited[i]=false;
-			}
-		}
-	}
-	private static void unSet(int cnt, int left, int right) {
-		if(right>left) {
-			return;
-		}
-		if(cnt==N) {
-			answer++;
-			return;
-		}
-		unSet(cnt+1, left+solArr[cnt], right);
-		unSet(cnt+1, left, right+solArr[cnt]);
-	}
+    static int N;
+    static int[] input;
+    static int[] num;
+    static int result;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            result=0;
+            input = new int[N];
+            num = new int[N];
+            visited = new boolean[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                input[i] = Integer.parseInt(st.nextToken());
+            }
+            permutation(0);
+            System.out.println("#"+t+" "+result);
+        }
+    }
+    private static void permutation(int cnt) {
+        if (cnt == N) {
+            subset(0,0,0);
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                num[cnt]=input[i];
+                permutation(cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+    private static void subset(int cnt, int right ,int left) {
+        if(right > left) {
+            return;
+        }
+        if (cnt == N) {
+            result++;
+            return;
+        }
+        subset(cnt+1, right+num[cnt], left);
+        subset(cnt+1, right, left+num[cnt]);
+    }
 }
