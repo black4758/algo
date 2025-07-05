b7d166404025d1fb1e62a42e7841d91b68e172d9
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-05 15:35:54 +0900

    수제 버거 장인

--- 240816/SWEA3421.java
+++ 240816/SWEA3421.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class SWE3421 {
+public class SWEA3421 {
     static int arr[][],n,m,an;
     static boolean visited[];
     public static void main(String[] args) {
