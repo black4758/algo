89c3121a49dd8c50fe7eef5e54a162f34bb5e9ba
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-05 15:56:38 +0900

    장훈이의 높은 선반

--- 240820/SWEA1486.java
+++ 240820/SWEA1486.java
@@ -1,35 +1,37 @@
+package main.java._2025_06_26;
+
 import java.util.Scanner;
 
 public class SWEA1486 {
-	static int [] arr;
-	static int N,H,sum;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			 N= sc.nextInt();
-			 H= sc.nextInt();
-			arr= new int[H];
-			for(int i=0;i<N;i++) {
-				arr[i]=sc.nextInt();
-			}
-			sum=Integer.MAX_VALUE; 
-			
-			jibhab(0,0);
-			System.out.println("#"+test_case+" "+sum-H);
-		}
-	}
-	private static void jibhab(int cnt,int max) {
-		if(max>=H) {
-			sum= Math.min(sum, max);
-			return;
-		}
-		if(cnt==N) {
-			return;
-		}
-		jibhab(cnt+1,max+arr[cnt]);
-		jibhab(cnt+1,max);
-	}
+    static int [] arr;
+    static int N,H,sum;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            N= sc.nextInt();
+            H= sc.nextInt();
+            arr= new int[N];
+            for(int i=0;i<N;i++) {
+                arr[i]=sc.nextInt();
+            }
+            sum=Integer.MAX_VALUE;
+
+            subset(0,0);
+            System.out.println("#"+test_case+" "+(sum-H));
+        }
+    }
+    private static void subset(int cnt,int max) {
+        if(max>=H) {
+            sum= Math.min(sum, max);
+            return;
+        }
+        if(cnt==N) {
+            return;
+        }
+        subset(cnt+1,max+arr[cnt]);
+        subset(cnt+1,max);
+    }
 }
