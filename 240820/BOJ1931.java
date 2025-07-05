0feda17ac02061ebd548dd60edd32206c089a974
black4758 <77867531+black4758@users.noreply.github.com>
2025-07-05 15:40:12 +0900

    회의실 배정

--- 240820/BOJ1931.java
+++ 240820/BOJ1931.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Boj1931 {
+public class BOJ1931 {
     static class Time{
         int start;
         int end;
