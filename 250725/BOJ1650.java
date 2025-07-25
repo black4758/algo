e0d9d6920b2e40d7c913c0f6ab0d7388545289c8
우성윤 <77867531+black4758@users.noreply.github.com>
2025-07-25 18:19:47 +0900

    어두운 건 무서워

---
+++ 250725/BOJ1650.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= C; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] +Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            long sum = (long)dp[x2][y2] - dp[x2][y1-1] -dp[x1-1][y2] +dp[x1-1][y1-1];
+            long ans = sum/((y2-y1+1) * (x2-x1+1));
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
