d1ec05adcead370f73aa62146a3265d58ee3957b
woo seong yun <arblack475867@gmail.com>
2025-09-12 23:39:03 +0900

    네트워크 연결

---
+++ 250912/BOJ1922_Kruskal.java
@@ -0,0 +1,60 @@
+package main.java._2025_09_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1922_Kruskal {
+    static int [] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        List<Node> list = new ArrayList<>();
+        arr = new int[N+1];
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list.add(new Node(start,end,cost));
+        }
+        list.sort(((o1, o2) -> o1.cost - o2.cost));
+        int cnt = 0;
+        int ans=0;
+        for(Node now : list) {
+            if(union(now.start,now.end)) {
+                ans+=now.cost;
+                if(++cnt == N-1) {
+                    break;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+    static int find(int x){
+        if(arr[x] == 0){
+            return x;
+        }
+        return arr[x]=find(arr[x]);
+    }
+
+    static boolean union(int a,int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot == bRoot){
+            return false;
+        }
+        arr[bRoot]=aRoot;
+        return true;
+    }
+    static class Node {
+        int  start,end,cost;
+        Node( int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+    }
+}
---
+++ 250912/BOJ1922_Prim.java
@@ -0,0 +1,55 @@
+package main.java._2025_09_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ1922_Prim {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        List<List<Node>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list.get(start).add(new Node(end, cost));
+            list.get(end).add(new Node(start, cost));
+        }
+        boolean[] visited = new boolean[N+1];
+        pq.offer(new Node(1, 0));
+        int ans = 0;
+        int cnt = 0;
+        while (!pq.isEmpty() && cnt < N) {
+            Node now = pq.poll();
+            if (!visited[now.end]) {
+                visited[now.end] = true;
+                ans+=now.cost;
+                cnt++;
+                for (Node next : list.get(now.end)) {
+                    if (!visited[next.end]) {
+                        pq.add(next);
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+    static class Node {
+        int  end,cost;
+        Node( int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+    }
+}
