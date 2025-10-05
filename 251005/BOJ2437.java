d676d7b40565dd629a15ac44d454d8a17f077252
woo seong yun <arblack475867@gmail.com>
2025-10-05 20:12:29 +0900

    저울

---
+++ 251005/BOJ2437.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ2437 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int ans = 1;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] >ans) {
+                break;
+            }
+            ans += arr[i];
+        }
+        System.out.println(ans);
+    }
+}
