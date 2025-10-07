76abe27df8dbc216eb994ed1977071cc5acf691b
woo seong yun <arblack475867@gmail.com>
2025-10-07 17:05:15 +0900

    LCS

---
+++ 251007/BOJ9251.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        int[][] arr = new int[input1.length()+1][input2.length()+1];
+        for (int i = 1; i <= input1.length(); i++) {
+            for (int j = 1; j <= input2.length(); j++) {
+                if (input1.charAt(i-1) == input2.charAt(j-1)) {
+                    arr[i][j] = arr[i-1][j-1] + 1  ;
+                }
+                else {
+                    arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+                }
+            }
+        }
+        System.out.println(arr[input1.length()][input2.length()]);
+    }
+}
