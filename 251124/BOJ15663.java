e2f9feefaa281e76b9d576056197810aad51b168
woo seong yun <black475867@gmail.com>
2025-11-24 12:51:27 +0900

    N과 M (9)

---
+++ 251124/BOJ15663.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15663 {
+    static int N,M;
+    static int[] arr;
+    static int[] ans;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        ans = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.print(sb.toString().trim());
+    }
+    static void dfs(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int prev = -1;
+        for (int i = 0; i < N; i++) {
+            if (!visited[i] && arr[i] != prev) {
+                visited[i] = true;
+                ans[cnt] = arr[i];
+                prev = arr[i];
+                dfs(cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
