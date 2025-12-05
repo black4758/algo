3b164f3c420dba488633fee4e5905cddba70854c
woo seong yun <arblack475867@gmail.com>
2025-12-05 18:00:00 +0900

    비밀번호 찾기

---
+++ 251205/BOJ17219.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ17219 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        Map<String, String> map = new HashMap<>();
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(br.readLine())).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
