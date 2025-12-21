8717bca331c090c33b1f6401ab2e52b4c4cd3f8d
black4758 <black475867@gmail.com>
2025-12-21 14:23:12 +0900

    수 정렬하기 3

---
+++ 251221/BOJ10989.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
